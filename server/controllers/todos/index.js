3d8582beb003bdf8305acfc1d280db402f4f76b3
mihanizm56 <m727507.56@mail.ru>
2021-10-12 16:07:02 +0300

    build(configure-project): update

--- server/controllers/todos/index.js
+++ server/controllers/todos/index.js
@@ -1,51 +1,55 @@
-const { todosModel } = require('../../models/todos');
 const { v4: uuidv4 } = require('uuid');
+const { todosModel } = require('../../models/todos');
 
 module.exports.getItems = async (req, res) => {
   const todos = await todosModel.getState();
 
   res.status(200).send(todos);
-}
+};
 
 module.exports.createItem = async (req, res) => {
   const { todo } = req.body;
   const id = uuidv4();
 
-  await todosModel.get('todos')
-    .unshift({ id: id, name: todo, checked: false})
-    .write()
+  await todosModel
+    .get('todos')
+    .unshift({ id, name: todo, checked: false })
+    .write();
 
-    res.status(200).send(todosModel.getState());
-}
+  res.status(200).send(todosModel.getState());
+};
 
 module.exports.deleteItem = async (req, res) => {
-  const { todoId }  = req.params;
+  const { todoId } = req.params;
 
-  await todosModel.get('todos')
-      .remove({ id: todoId })
-      .write()
+  await todosModel
+    .get('todos')
+    .remove({ id: todoId })
+    .write();
 
-    res.status(200).send(todosModel.getState());
-}
+  res.status(200).send(todosModel.getState());
+};
 
 module.exports.markItem = async (req, res) => {
   const { todoId } = req.params;
 
-  await todosModel.get('todos')
+  await todosModel
+    .get('todos')
     .find({ id: todoId })
-    .assign({ checked: true})
-    .write()
+    .assign({ checked: true })
+    .write();
 
   res.status(200).send(todosModel.getState());
-}
+};
 
 module.exports.unmarkItem = async (req, res) => {
   const { todoId } = req.params;
 
-  await todosModel.get('todos')
+  await todosModel
+    .get('todos')
     .find({ id: todoId })
-    .assign({ checked: false})
-    .write()
+    .assign({ checked: false })
+    .write();
 
   res.status(200).send(todosModel.getState());
-}
+};
--- server/models/todos/index.js
+++ server/models/todos/index.js
@@ -2,16 +2,11 @@ const path = require('path');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const dataJSONFilePath = path.join(
-  'server',
-  'models',
-  'todos',
-  'db.json',
-);
+const dataJSONFilePath = path.join('server', 'models', 'todos', 'db.json');
 
 const adapter = new FileSync(dataJSONFilePath);
 const db = low(adapter);
 
-db.defaults({todos: []}).write();
+db.defaults({ todos: [] }).write();
 
 module.exports.todosModel = db;
