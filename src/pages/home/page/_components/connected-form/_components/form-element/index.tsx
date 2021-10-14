33066fb850d2770f422502f81a209c2ceec0e6f9
mihanizm56 <m727507.56@mail.ru>
2021-10-14 13:36:29 +0300

    build(configure-project): build form

--- src/pages/home/page/_components/connected-form/_components/form-element/index.tsx
+++ src/pages/home/page/_components/connected-form/_components/form-element/index.tsx
@@ -1,6 +1,6 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo } from 'react';
 import classnames from 'classnames/bind';
-import { Form } from 'react-final-form';
+import { Field, Form } from 'react-final-form';
 import {
   BasicPlusIcon,
   ButtonLink,
@@ -12,58 +12,22 @@ const cn = classnames.bind(styles);
 const BLOCK_NAME = 'Form-element';
 
 type PropsType = {
-  onAddTodo: () => void;
-  errorText: string;
-  disabled: boolean;
-  value: string;
-  onChange: (event: string) => void;
-  onFocus: () => void;
-  onBlur: () => void;
+  onAddTodo: ({ todo }: { todo: string }) => void;
 };
 
-export const FormElement = memo(
-  ({
-    onAddTodo,
-    errorText,
-    disabled,
-    value,
-    onChange,
-    onFocus,
-    onBlur,
-  }: PropsType) => {
-    const makePropsForInput = useCallback(() => {
-      return {
-        onBlur,
-        onChange,
-        onFocus,
-        name: 'todo',
-        type: 'text',
-        value,
-      };
-    }, [onBlur, onChange, onFocus, value]);
+export const FormElement = memo(({ onAddTodo }: PropsType) => {
+  return (
+    <div className={cn(BLOCK_NAME)}>
+      <Form
+        onSubmit={onAddTodo}
+        render={({ handleSubmit }) => (
+          <form className={cn(`${BLOCK_NAME}__form`)} onSubmit={handleSubmit}>
+            <Field component={FormSimpleInput} name="todo" />
 
-    return (
-      <div className={cn(BLOCK_NAME)}>
-        <Form
-          onSubmit={onAddTodo}
-          render={({ handleSubmit }) => (
-            <form className={cn(`${BLOCK_NAME}__form`)} onSubmit={handleSubmit}>
-              <FormSimpleInput
-                id="todo-input"
-                input={makePropsForInput()}
-                meta={{ error: `${errorText}` }}
-                name="todo"
-                placeholder="Add Todo"
-              />
-              <ButtonLink
-                disabled={disabled}
-                rightIcon={BasicPlusIcon}
-                type="submit"
-              />
-            </form>
-          )}
-        />
-      </div>
-    );
-  },
-);
+            <ButtonLink rightIcon={BasicPlusIcon} type="submit" />
+          </form>
+        )}
+      />
+    </div>
+  );
+});
--- src/pages/home/page/_components/connected-form/index.tsx
+++ src/pages/home/page/_components/connected-form/index.tsx
@@ -16,36 +16,7 @@ type StateType = {
 };
 
 class WrappedContainer extends Component<PropsType, StateType> {
-  constructor(props) {
-    super(props);
-    this.state = { todoInputValue: '', errorText: '', disabled: false };
-  }
-
-  handleTodoInputChange = (event: string) => {
-    if (event.match(/[^a-zA-Zа-яА-ЯёË\s-]/gi)) {
-      this.setState({ errorText: 'Можно ввести только буквы', disabled: true });
-    } else if (event.length < 10) {
-      this.setState({ errorText: 'Минимум 10 символов', disabled: true });
-    } else if (event.length > 20) {
-      this.setState({ errorText: 'Максимум 20 символов', disabled: true });
-    } else {
-      this.setState({ errorText: '', disabled: false });
-    }
-
-    this.setState({ todoInputValue: event });
-  };
-
-  handleTodoInputBlur = () => {
-    this.setState({ errorText: '' });
-  };
-
-  handleTodoInputFocus = () => {
-    if (this.state.todoInputValue.length === 0) {
-      this.setState({ errorText: 'Заполните поле', disabled: true });
-    }
-  };
-
-  handleAddTodo = () => {
+  handleAddTodo = ({ todo }: { todo: string }) => {
     this.props.submitForm({
       formRequest: postNewTodo,
       formSuccessAction: fetchTodosSuccess,
@@ -58,17 +29,7 @@ class WrappedContainer extends Component<PropsType, StateType> {
   };
 
   render() {
-    return (
-      <FormElement
-        disabled={this.state.disabled}
-        errorText={this.state.errorText}
-        onAddTodo={this.handleAddTodo}
-        onBlur={this.handleTodoInputBlur}
-        onChange={this.handleTodoInputChange}
-        onFocus={this.handleTodoInputFocus}
-        value={this.state.todoInputValue}
-      />
-    );
+    return <FormElement onAddTodo={this.handleAddTodo} />;
   }
 }
 
